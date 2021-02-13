7d060f558c5d5b46fe2c3ba16475a9e9668a4bad
Justin Mitchell <71776129+MitchellTesla@users.noreply.github.com>
2021-02-13 10:16:20 -0500

    Add repo for testing and development

---
+++ Ξ/mitchelltesla/docs/sol/smart-contracts/Ownable.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyContract is Ownable {
+    function normalThing() public {
+        // anyone can call this normalThing()
+    }
+
+    function specialThing() public onlyOwner {
+        // only the owner can call specialThing()!
+    }
+}
